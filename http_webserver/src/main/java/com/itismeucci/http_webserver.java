f5898ffcc6e214a01ef4a5e2598a61eb951cd9e6
lai2-01 <lai2-01@itismeucci.com>
2021-12-15 12:07:08 +0100

    Creato progetto

---
+++ http_webserver/src/main/java/com/itismeucci/http_webserver.java
@@ -0,0 +1,13 @@
+package com.itismeucci;
+
+/**
+ * Hello world!
+ *
+ */
+public class http_webserver 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
